1efdbd37ee2031990a8dbdd69b4484f74774ef0c
Mazen Mohamed <kidkever@yahoo.com>
2021-06-25 23:38:25 +0200

    project models and property views

--- property/admin.py
+++ property/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Property, PropertyBook, PropertyImages, PropertyReview, Category, Place
+
+admin.site.register(Property)
+admin.site.register(PropertyBook)
+admin.site.register(PropertyImages)
+admin.site.register(PropertyReview)
+admin.site.register(Category)
+admin.site.register(Place)
