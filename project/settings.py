89165725cda8765c70c7d7929b62354b5238671a
Mazen Mohamed <kidkever@yahoo.com>
2021-08-03 23:26:57 +0200

    adding users - edit and profile

--- project/settings.py
+++ project/settings.py
@@ -32,6 +32,8 @@ ALLOWED_HOSTS = ['mazen-airbnb.herokuapp.com', '127.0.0.1']
 # Application definition
 
 INSTALLED_APPS = [
+    "accounts",
+
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
@@ -39,7 +41,6 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     # my apps
-    "accounts",
     "blog",
     "property",
     "about",
